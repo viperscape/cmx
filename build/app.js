65b11e55cb796c0428248ee06c567bcb7252e86d
Chris Gill <chrisfgill@gmail.com>
2022-03-04 06:02:56 -0500

    update for vscode and remove generated files

--- build/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const server = require('./server');
-const db = require('./db');
-const store = new db.Store('store.db');
-const moment = require('moment');
-const app = server.start(9999, store, function (n) {
-    let time = moment(n.seenTime).add(4, "hour").format('YYYY-MM-DD HH:mm:ss');
-    store.update(n.clientMac, time);
-});
